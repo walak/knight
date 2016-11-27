c619031505a4e32a7b5a7ade7fd184d7f53163ce
walák <feniksy@gmail.com>
2016-11-27 21:17:50 +0100

    No multithread for now

--- knight.py
+++ knight.py
@@ -1,4 +1,5 @@
 import time
+from threading import Thread
 
 from engine import get_random_location, get_next_random_possible_move, move
 from model import Knight, Board, MoveHistory
@@ -30,10 +31,20 @@ def simulate_until_to_list(condition, history_list):
         history_list.append(simulate_once())
 
 
+class Simulator(Thread):
+    def __init__(self, condition):
+        super().__init__()
+        self.results = []
+        self.condition = condition
+
+    def run(self):
+        simulate_until_to_list(self.condition, self.results)
+        return
+
 
 if __name__ == "__main__":
-    history = []
     start = time.time()
     condition = lambda: time.time() - start <= 1.0
+
     history = simulate_until(condition)
     print(len(history))
