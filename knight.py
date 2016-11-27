fc4d2ebb94aa24075f7bb06c639489b1bb227812
walák <feniksy@gmail.com>
2016-11-27 20:35:10 +0100

    Preparing for multithreaded environment

--- knight.py
+++ knight.py
@@ -1,21 +1,39 @@
-from model import Knight, Board, MoveHistory, Move
+import time
+
 from engine import get_random_location, get_next_random_possible_move, move
+from model import Knight, Board, MoveHistory
 
 
 def create_random_knight():
     return Knight(*(get_random_location(Board.BOARD_SIZE)))
 
 
+def simulate_once():
+    board = Board()
+    knight = create_random_knight()
+    moves_so_far = []
+    next_move = get_next_random_possible_move(knight, board)
+    while next_move is not None:
+        moves_so_far.append(move(knight, board, next_move))
+        next_move = get_next_random_possible_move(knight, board)
+    return MoveHistory(moves_so_far, knight, board)
+
+
+def simulate_until(condition=lambda: True):
+    history = []
+    simulate_until_to_list(condition, history)
+    return history
+
+
+def simulate_until_to_list(condition, history_list):
+    while condition():
+        history_list.append(simulate_once())
+
+
+
 if __name__ == "__main__":
     history = []
-    while True:
-        board = Board()
-        knight = create_random_knight()
-        moves_so_far = []
-        next_move = get_next_random_possible_move(knight, board)
-        while next_move is not None:
-            moves_so_far.append(move(knight, board, next_move))
-            next_move = get_next_random_possible_move(knight, board)
-        current_history = MoveHistory(moves_so_far, knight, board)
-        history.append(current_history)
-    pass
+    start = time.time()
+    condition = lambda: time.time() - start <= 1.0
+    history = simulate_until(condition)
+    print(len(history))
